d083baaf370c521f8198b30ea856b1f12d54f693
Bharti <bharatifule10gmail.com>
2023-01-31 14:00:32 +0530

    Added:LinkedList Problem

---
+++ bridglabz/datastructure/Node.java
@@ -0,0 +1,15 @@
+package com.bridglabz.datastructure;
+
+public class Node<T> {
+	 T key;
+	    Node<T> next;
+
+	    public Node() {
+
+	    }
+
+	    public Node(T key ) {
+	        this.key = key;
+
+	    }
+}
