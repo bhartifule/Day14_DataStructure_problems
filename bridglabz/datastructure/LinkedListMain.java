459763c4f0449c5947e900629da27c2f436f72b1
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 23:27:58 +0530

    Update LinkedListMain.java

--- bridglabz/datastructure/LinkedListMain.java
+++ bridglabz/datastructure/LinkedListMain.java
@@ -3,7 +3,8 @@ package com.bridglabz.datastructure;
 import java.util.LinkedList;
 
 public class LinkedListMain {
-	 public static void main(String[] args) {
+	 public static void main(String[] args)
+	 {
 	        LinkedList<Integer> linkedList1 = new LinkedList<>();
 	        linkedList1.push(70);
 	        linkedList1.push(30);
